fc999ea422cfb394e58d1c71ab4fd224ea7d3658
Jason Blevins <jrblevin@sdf.org>
2017-07-17 09:24:46 -0500

    Add release notes

---
+++ changes.el
@@ -0,0 +1,27 @@
+;; Support code for updating release notes
+;;
+;; Jason Blevins <jrblevin@sdf.org>
+
+(defun markdown-mode-guide-changes ()
+  "Retrieve and process contents of CHANGES.md."
+  (let* ((url "https://raw.githubusercontent.com/jrblevin/markdown-mode/master/CHANGES.md")
+         (buf (url-retrieve-synchronously url t t)))
+    (when buf
+      (with-current-buffer buf
+        ;; Convert H1 to H2.
+        (while (re-search-forward "^#" nil t 1)
+          (replace-match "##" nil nil))
+        (goto-char (point-min))
+        ;; Convert <kbd> tags to Markdown inline code.
+        (while (re-search-forward markdown-regex-kbd nil t 1)
+          (replace-match (concat "`" (match-string 2) "`") nil nil))
+        ;; Return everything but HTTP headers.
+        (goto-char (point-min))
+        (search-forward "\n\n")
+        (buffer-substring (point) (point-max))))))
+
+(defun markdown-mode-guide-insert-changes ()
+  "Insert processed contents of CHANGES.md."
+  (interactive)
+  (let ((changes (markdown-mode-guide-changes)))
+    (insert changes)))
