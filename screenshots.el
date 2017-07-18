529c3a3afb0e329d6c27455a1245b055e463760c
Jason Blevins <jrblevin@sdf.org>
2017-07-18 15:26:53 -0500

    Set frame style in screenshot.el

--- screenshots.el
+++ screenshots.el
@@ -10,6 +10,19 @@
 ;;    -W      capture window
 ;;    -l      window id
 
+;; Set up a consistent look for screenshots.
+(setq-default line-spacing 0.25)
+(set-face-attribute 'default nil :family "Fira Code" :height 160)
+(set-face-attribute 'fixed-pitch nil :family "Source Code Pro")
+(set-face-attribute 'variable-pitch nil :family "Fira Sans")
+(dolist (theme custom-enabled-themes)
+  (disable-theme theme))
+(load-theme 'sanityinc-tomorrow-day)
+(set-frame-size (selected-frame) 80 35)
+
+;; Load markdown-mode
+(load-library "markdown-mode.el")
+
 (defconst markdown-guide-images-dir "~/work/markdown-mode-guide/manuscript/images/")
 
 (defun markdown-guide-screenshot (&optional filename)
@@ -22,3 +35,5 @@
   (let ((window-id
          (do-applescript "tell app \"Emacs\" to id of window 1")))
     (shell-command (format "screencapture -o -x -tpng -T1 -W -l%d %s" window-id filename))))
+
+(global-set-key (kbd "C-c s") #'markdown-guide-screenshot)
